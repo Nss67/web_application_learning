aac7422dc157ca0b32420ac5bbb5539d8160e00b
Nss67 <milade67@hotmail.com>
2025-03-31 07:12:54 +0330

    I create first models for my shop

--- django_tutorial/shop/models.py
+++ django_tutorial/shop/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Customers(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=500, default="", blank=True, null=True)
+    price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to="upload/products/")
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Orders(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=400, default="", blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    date = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.product
