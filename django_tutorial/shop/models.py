fa5146553f8b6025dda944ed007d987f84f97a7e
Nss67 <milade67@hotmail.com>
2025-04-07 18:41:46 +0330

    jinja for if/else block code

--- django_tutorial/shop/models.py
+++ django_tutorial/shop/models.py
@@ -30,6 +30,9 @@ class Products(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to="upload/products/")
 
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+
 
     def __str__(self):
         return self.name
